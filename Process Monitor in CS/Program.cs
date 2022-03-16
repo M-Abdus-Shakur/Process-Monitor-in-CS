9a0394b75c7eca0aa07ef59c683856ae3d7e263f
M-Abdus-Shakur <mabdushshakur12@gmail.com>
2022-03-16 13:11:46 +0600

    Add project files.

---
+++ Process Monitor in CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+namespace Process_Monitor_in_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] proclist = Process.GetProcesses();
+            foreach (Process process in proclist)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Process : " + process.ProcessName);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("PID : " + process.Id);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("WindowTitle : " + process.MainWindowTitle);
+            }
+            Console.ReadLine();
+        }
+    }
+}
